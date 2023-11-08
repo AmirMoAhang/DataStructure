db4e5510beaac3b14845a6caa5f25236d1abd3af
Amir.Mo.Ahang <amir.mo.ahang@gmail.com>
2023-11-08 21:28:46 +0330

    add tower of hanoi

---
+++ Recursive functions/TowerOfHanoi.py
@@ -0,0 +1,11 @@
+def Hanoi(numberOfDisks, fromRod, toRod, auxRod):
+    if numberOfDisks == 1:
+        print(f"disk {numberOfDisks} moved from Rod {fromRod}, to Rod {toRod}")
+        return
+    Hanoi(numberOfDisks - 1, fromRod=fromRod, toRod = auxRod, auxRod = toRod)
+    print(f"disk {numberOfDisks} moved from Rod {fromRod}, to Rod {toRod}")
+    Hanoi(numberOfDisks - 1, fromRod = auxRod, toRod=toRod, auxRod= fromRod)
+    
+    
+    
+Hanoi(3, 'A', 'c', 'B')
